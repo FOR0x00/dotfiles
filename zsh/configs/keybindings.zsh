80920e090824b4e37a1f2e3ebcd08c8ecec59bfa
FOR0x00 <zhuimengmayi@gmail.com>
2026-04-17 17:26:03 +0800

    add more config

--- zsh/configs/keybindings.zsh
+++ zsh/configs/keybindings.zsh
@@ -5,7 +5,8 @@ bindkey -v
 bindkey -M vicmd "H" vi-beginning-of-line
 bindkey -M vicmd "L" vi-end-of-line
 
-function wrap_clipboard_widgets() {
+# Clipboard
+wrap_clipboard_widgets() {
   local widget
   local wrapped_name
   for widget in "$@"; do
@@ -19,35 +20,47 @@ function wrap_clipboard_widgets() {
     zle -N "${widget}" "${wrapped_name}"
   done
 }
-
 wrap_clipboard_widgets vi-yank vi-yank-eol vi-yank-whole-line
 
-# https://github.com/LukeSmithxyz/voidrice/blob/master/.config/zsh/.zshrc#L40-L51
 # Change cursor shape for different vi modes.
-function zle-keymap-select () {
-    case $KEYMAP in
-        vicmd) echo -ne '\e[1 q';;      # block
-        viins|main) echo -ne '\e[5 q';; # beam
-    esac
+_cursor_block() { printf '\e[1 q'; }
+_cursor_beam()  { printf '\e[5 q'; }
+
+zle-keymap-select () {
+  case $KEYMAP in
+    vicmd) _cursor_block ;;
+    *) _cursor_beam ;;
+  esac
+}
+zle-line-init() {
+  case $KEYMAP in
+    vicmd) _cursor_block ;;
+    *) _cursor_beam ;;
+  esac
+}
+zle-line-finish() {
+  _cursor_block
 }
 zle -N zle-keymap-select
-zle-line-init() {
-    zle -K viins # initiate `vi insert` as keymap (can be removed if `bindkey -V` has been set elsewhere)
-    echo -ne "\e[5 q"
-}
 zle -N zle-line-init
-echo -ne '\e[5 q' # Use beam shape cursor on startup.
-preexec() { echo -ne '\e[5 q' ;} # Use beam shape cursor for each new prompt.
+zle -N zle-line-finish
+# hooks
+autoload -Uz add-zsh-hook
+_cursor_preexec() { _cursor_beam; }
+_cursor_precmd()  { _cursor_beam; }
+add-zsh-hook preexec _cursor_preexec
+add-zsh-hook precmd _cursor_precmd
 
-# autoload edit-command-line; zle -N edit-command-line
-# bindkey '^e' edit-command-line
-# bindkey -M vicmd '^e' edit-command-line
+# Widget
+autoload edit-command-line; zle -N edit-command-line
+bindkey '^E' edit-command-line
+bindkey -M vicmd '^E' edit-command-line
 
-function lazygit_widget() {
+lazygit_widget() {
   zle -I
   command lazygit
   zle reset-prompt
 }
 zle -N lazygit_widget
-bindkey '^g' lazygit_widget
+bindkey '^G' lazygit_widget
 
